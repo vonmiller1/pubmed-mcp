0f8d1031ea0fa91daf2229456cb6d820819f5d18
Chris Mannina <chrismannina@outlook.com>
2023-09-22 12:00:00 +0000

    style: fix Black formatting issues in test_server.py

--- tests/test_server.py
+++ tests/test_server.py
@@ -26,7 +26,7 @@ class TestPubMedMCPServer:
         assert server.tool_handler is not None
         assert server.cache is not None
         assert server.server is not None
-        assert hasattr(server, 'get_cache_stats')
+        assert hasattr(server, "get_cache_stats")
 
     def test_server_initialization_with_defaults(self):
         """Test server initialization with default parameters."""
@@ -60,10 +60,10 @@ class TestPubMedMCPServer:
         )
 
         # Check that all components exist
-        assert hasattr(server, 'pubmed_client')
-        assert hasattr(server, 'tool_handler')
-        assert hasattr(server, 'cache')
-        assert hasattr(server, 'server')
+        assert hasattr(server, "pubmed_client")
+        assert hasattr(server, "tool_handler")
+        assert hasattr(server, "cache")
+        assert hasattr(server, "server")
 
         # Check that tool handler has the pubmed client
         assert server.tool_handler.pubmed_client is server.pubmed_client
@@ -81,7 +81,7 @@ class TestPubMedMCPServer:
         tools = server.tool_handler.get_tools()
         assert isinstance(tools, list)
         assert len(tools) > 0
-        
+
         # Check that each tool has required fields
         for tool in tools:
             assert "name" in tool
@@ -97,18 +97,19 @@ class TestPubMedMCPServer:
 
         # Mock the tool handler response
         mock_response = MCPResponse(
-            content=[{"type": "text", "text": "Test result"}],
-            is_error=False
+            content=[{"type": "text", "text": "Test result"}], is_error=False
         )
 
-        with patch.object(server.tool_handler, 'handle_tool_call', return_value=mock_response):
+        with patch.object(server.tool_handler, "handle_tool_call", return_value=mock_response):
             # This tests the integration but doesn't call the actual MCP server handlers
             # since those are registered during initialization
-            result = await server.tool_handler.handle_tool_call("search_pubmed", {"query": "cancer"})
+            result = await server.tool_handler.handle_tool_call(
+                "search_pubmed", {"query": "cancer"}
+            )
             assert result.content[0]["text"] == "Test result"
             assert result.is_error is False
 
-    @pytest.mark.asyncio  
+    @pytest.mark.asyncio
     async def test_tool_call_error_handling(self):
         """Test error handling in tool calls."""
         server = PubMedMCPServer(
@@ -117,7 +118,9 @@ class TestPubMedMCPServer:
         )
 
         # Mock the tool handler to raise an exception
-        with patch.object(server.tool_handler, 'handle_tool_call', side_effect=Exception("Test error")):
+        with patch.object(
+            server.tool_handler, "handle_tool_call", side_effect=Exception("Test error")
+        ):
             # Test that errors are properly handled
             with pytest.raises(Exception) as exc_info:
                 await server.tool_handler.handle_tool_call("invalid_tool", {})
@@ -132,7 +135,7 @@ class TestPubMedMCPServer:
         )
 
         # Mock the client close method
-        with patch.object(server.pubmed_client, 'close') as mock_close:
+        with patch.object(server.pubmed_client, "close") as mock_close:
             await server.shutdown()
             mock_close.assert_called_once()
 
@@ -144,4 +147,4 @@ class TestPubMedMCPServer:
         )
 
         assert server.pubmed_api_key == "test_key"
-        assert server.pubmed_email == "test@example.com" 
+        assert server.pubmed_email == "test@example.com"
